d411f87ca1318638c3b05d2ff1ec97b95d9521a7
svaronc <sebastianvaron96@gmail.com>
2023-12-10 18:39:07 +0000

    refactor function to get all reservations from one user

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -59,11 +59,10 @@ const getUserWithId = function (id) {
  */
 const addUser = function (user) {
   return pool
-    .query(`INSERT INTO users (name,email,password) VALUES ($1,$2,$3) RETURNING *`, [
-      user.name,
-      user.email,
-      user.password,
-    ])
+    .query(
+      `INSERT INTO users (name,email,password) VALUES ($1,$2,$3) RETURNING *`,
+      [user.name, user.email, user.password]
+    )
     .then((result) => {
       console.log(result.rows);
       return result.rows;
@@ -85,7 +84,31 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT reservations.*,properties.*,property_reviews.*
+  FROM
+    reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE
+    reservations.guest_id = $1
+  GROUP BY
+    properties.id,
+    reservations.id
+  ORDER BY
+    reservations.start_date
+  LIMIT
+    $2;`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
